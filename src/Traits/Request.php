73ff5533a812d286ba3e9f8e1fd3ceb1fce3ca2d
Mansu Jeong <pronist@naver.com>
2018-12-18 04:51:41 +0900

    modify: 'Request' Trait

--- src/Traits/Request.php
+++ src/Traits/Request.php
@@ -9,6 +9,9 @@ use Tistory\Exceptions\BadResponseException;
 use Tistory\Exceptions\FileUploadException;
 
 /**
+ * @method stdClass getOptions($accessToken, $query = [], $filename = null)
+ * @method stdClass get($url, $accessToken, $query = [])
+ * @method stdClass post($url, $accessToken, $query = [], $filename = null)
  * @method stdClass request($method, $url, $accessToken, $query = [], $filename = null)
  */
 trait Request
@@ -17,6 +20,115 @@ trait Request
     * @var string $httpClient GuzzleHttp Client
     */
     public static $httpClient;
+
+   /**
+    * Getting request options
+    *
+    * @param string $accessToken
+    * @param array $query
+    * @param string $filename
+    *
+    * @return array
+    */
+    public static function getOptions(
+        string $accessToken, 
+        array $query = [], 
+        string $filename = null)
+    {
+        $requestOptions = [
+            'query' => array_merge([
+                'access_token' => $accessToken,
+                'output' => 'json',
+            ], $query)
+        ];
+        if($filename) {
+            /** file upload? */
+            $requestOptions = array_merge($requestOptions, [
+                'multipart' => [
+                    [
+                        'name' => 'uploadedfile',
+                        'contents' => fopen($filename, 'r')
+                    ]
+                ]
+            ]);
+        }
+        return $requestOptions;
+    }
+
+   /**
+    * Get
+    *
+    * @param string $url
+    * @param string $accessToken
+    * @param array $query
+    *
+    * @return stdClass
+    */
+    public static function get(
+        string $url,
+        string $accessToken,
+        array $query = [])
+    {
+        try {
+            $response = json_decode(
+                Request::$httpClient->get(
+                    $url, 
+                    Request::getOptions($accessToken, $query)
+                )->getBody(), true)
+            ;
+            return (object) $response['tistory']['item'];
+        }
+        catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                throw new BadResponseException("Bad request", 400);
+            }
+        }
+    }
+
+   /**
+    * Post
+    *
+    * @param string $url
+    * @param string $accessToken
+    * @param array $query
+    * @param string $filename
+    *
+    * @return stdClass
+    */
+    public static function post(
+        string $url,
+        string $accessToken,
+        array $query = [],
+        string $filename = null)
+    {
+        try {
+            $response = json_decode(
+                Request::$httpClient->post(
+                    $url, 
+                    Request::getOptions($accessToken, $query, $filename)
+                )->getBody(), true)
+            ;
+            $result = [];
+
+            /** without 'status' */
+            foreach($response['tistory'] as $key => $value) {
+                if($key != 'status') {
+                    $result[$key] = $value;
+                }
+            }
+            return (object) $result;
+        }
+        catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                if($filename) {
+                    throw new FileUploadException("The file ${filename} upload failed.", 400);
+                }
+                else {
+                    throw new BadResponseException("Bad request", 400);
+                }
+            }
+        }
+    }
 
    /**
     * Tistory API Request
@@ -29,63 +141,14 @@ trait Request
     *
     * @return stdClass
     */
-    private static function request(
+    public static function request(
         string $method, 
         string $url,
         string $accessToken,
         array $query = [],
         string $filename = null)
     {
-        try {
-
-            $requestOptions = [
-                'query' => array_merge([
-                    'access_token' => $accessToken,
-                    'output' => 'json',
-                ], $query)
-            ];
-
-            if($method == 'post' && $filename) {
-                /** file upload? */
-                $requestOptions = array_merge($requestOptions, [
-                    'multipart' => [
-                        [
-                            'name' => 'uploadedfile',
-                            'contents' => fopen($filename, 'r')
-                        ]
-                    ]
-                ]);
-            }
-            
-            $response = json_decode(Request::$httpClient->$method($url, $requestOptions)->getBody(), true);
-
-            switch($method) {
-                case 'get':
-                    return (object) $response['tistory']['item'];
-                    break;
-                case 'post':
-                    $result = [];
-
-                    /** without 'status' */
-                    foreach($response['tistory'] as $key => $value) {
-                        if($key != 'status') {
-                            $result[$key] = $value;
-                        }
-                    }
-                    return (object) $result;
-                    break;
-            }
-        }
-        catch (RequestException $e) {
-            if ($e->hasResponse()) {
-                if($filename) {
-                    throw new FileUploadException("The file ${filename} upload failed.", 400);
-                }
-                else {
-                    throw new BadResponseException("Bad request", 400);
-                }
-            }
-        }
+        return Request::$method($url, $accessToken, $query, $filename);
     }
 }
 
